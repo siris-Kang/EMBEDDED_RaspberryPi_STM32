72f7f5f0f0cb430ca237be0742928a1e589ebb92
siris-Kang <kke3436@naver.com>
2025-11-27 11:12:46 +0900

    [Upload] RC car project

---
+++ RCcar/STM_Motor/SMTMotor.md
@@ -0,0 +1,11 @@
+# STM 보드로 모터 구동하기
+
+
+## 1. Usart Test
+uart 통신 확인 코드 구현  
+CubeIDE를 통해서 NUCLEO-F103RB에 업로드  
+라즈베리파이와 연결 후  통신 확인  
+
+<img src="./images/image_uarttest.png"> 
+
+
---
+++ RCcar/STM_Motor/uart_test.c
@@ -0,0 +1,84 @@
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  char *start_msg = "STM32 UART Test Start\r\n";
+  HAL_UART_Transmit(&huart2, (uint8_t*)start_msg, strlen(start_msg), 100);
+
+  uint32_t last_tick = HAL_GetTick();
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    // 1) 1초에 한 번 상태 메시지 전송
+    if (HAL_GetTick() - last_tick >= 1000) {
+      last_tick = HAL_GetTick();
+      char *msg = "STM32 UART OK\r\n";
+      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);
+    }
+
+    // 2) 수신 데이터가 있으면 1바이트 읽어서 다시 에코
+    uint8_t ch;
+    if (HAL_UART_Receive(&huart2, &ch, 1, 10) == HAL_OK) {
+      // 받은 글자를 다시 전송 (에코)
+      HAL_UART_Transmit(&huart2, &ch, 1, 10);
+    }
+
+    // 너무 바쁘게 돌지 않게 약간 쉼
+    HAL_Delay(1);
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+
+/*
+// usart.c
+#include "usart.h"
+
+UART_HandleTypeDef huart2;
+
+void MX_USART2_UART_Init(void)
+{
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+*/
